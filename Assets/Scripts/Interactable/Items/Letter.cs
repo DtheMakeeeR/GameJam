4691dc08cdc52d59eaf207a41a70fb6b572e8afa
DtheMakeeeR <danilgol45@mail.ru>
2026-07-25 15:17:27 +0300

    start level 1

--- Assets/Scripts/Interactable/Items/Letter.cs
+++ Assets/Scripts/Interactable/Items/Letter.cs
@@ -4,6 +4,10 @@ public class Letter : Interactble
 {
     [SerializeField] private GameObject _panel;
 
+    private void Awake()
+    {
+        _panel = GameObject.FindGameObjectWithTag("LetterPanel");
+    }
     public override void MakeInteraction(PlayerController player)
     {
         _panel.SetActive(true);
